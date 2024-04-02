a9eb01fb7d2c842d30c53d0a9a674e5a931e7e2b
BinoManjesh <binomanjesh@gmail.com>
2024-04-02 22:42:40 +0530

    Add nav bar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,28 @@
-function Root() {}
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import styles from "../styles/root.module.css";
+import CartIcon from "../assets/cart.svg";
+
+function Root() {
+  const [cart] = useState(new Map());
+  let numItems = 0;
+  for (const [, value] of cart) {
+    numItems += value;
+  }
+  return (
+    <>
+      <nav className={styles.header}>
+        <Link to="." className={styles.logo} aria-label="Home page">
+          <img src="/icon.png" alt="Website logo" />
+        </Link>
+        <Link to="cart" className={styles.cart} aria-label="Shopping cart">
+          <img src={CartIcon} alt="" />
+        </Link>
+        <p>{numItems}</p>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
 
 export default Root;
