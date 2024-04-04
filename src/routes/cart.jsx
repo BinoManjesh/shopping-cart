3537369c070c007938e4247ecffabeef70f4a50b
BinoManjesh <binomanjesh@gmail.com>
2024-04-04 10:50:55 +0530

    Add shopping cart page

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,3 +1,38 @@
-function Cart() {}
+import { Fragment } from "react";
+import { useOutletContext } from "react-router-dom";
+import Product from "../components/Product";
+import CartManager from "../components/CartManager";
+import styles from "../styles/cart.module.css";
+
+function Cart() {
+  const { cart, addToCart } = useOutletContext();
+  let total = 0;
+  return (
+    <div className={styles.container}>
+      <h2>Product</h2>
+      <h2>Price</h2>
+      <hr />
+      {Array.from(cart.values()).map((value) => {
+        const price = value.count * value.product.price;
+        total += price;
+        return (
+          <Fragment key={value.product.id}>
+            <Product product={value.product}>
+              <CartManager
+                cart={cart}
+                addToCart={addToCart}
+                product={value.product}
+              />
+            </Product>
+            <p>$ {price}</p>
+            <hr />
+          </Fragment>
+        );
+      })}
+      <h2>Total: </h2>
+      <p>$ {total}</p>
+    </div>
+  );
+}
 
 export default Cart;
